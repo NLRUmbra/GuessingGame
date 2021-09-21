00e7600e7a03924525885af54e2c9c45a70bfde5
danhuynh1 <danh8953@gmail.com>
2021-09-21 14:38:13 -0400

    Update GuessingGameDaoImpl.java

--- GuessTheNumber/src/main/java/Main/Dao/GuessingGameDaoImpl.java
+++ GuessTheNumber/src/main/java/Main/Dao/GuessingGameDaoImpl.java
@@ -5,10 +5,41 @@
  */
 package Main.Dao;
 
+import Main.CTO.Game;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  *
  * @author Noah McElroy
  */
-public class GuessingGameDaoImpl {
+public class GuessingGameDaoImpl implements GuessingGameDao{
+    private Map<int, Game> gameList;
+
+    public GuessingGameDaoImpl() {
+    }
+
+    @Override
+    public Game addGame(int id, Game game) {
+       gameList.put(game.getId(),game);
+       return gameList.get(game.getId());
+    }
+
+    @Override
+    public Game deleteGame(int id) {
+        return gameList.remove(id);
+    }
+
+    @Override
+    public Game updateGame(int id) {
+        
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Game> listAllGames() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
