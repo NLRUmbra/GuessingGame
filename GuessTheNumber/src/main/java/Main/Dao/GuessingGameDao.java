2563e7aa24d3258708fe30b51634c7b008308b6d
NLRUmbra <56780974+NLRUmbra@users.noreply.github.com>
2021-09-21 14:28:56 -0400

    Update GuessingGameDao.java

--- GuessTheNumber/src/main/java/Main/Dao/GuessingGameDao.java
+++ GuessTheNumber/src/main/java/Main/Dao/GuessingGameDao.java
@@ -10,5 +10,11 @@ package Main.Dao;
  * @author Noah McElroy
  */
 public class GuessingGameDao {
+    Game addGame(int id, Game game);
     
+    Game deleteGame(int id);
+    
+    Game updateGame(int id);
+    
+    List<Game> listAllGames();
 }
