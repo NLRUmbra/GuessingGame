0ca6cd45e18cc6bc87417c84ad55b12ee6333464
danhuynh1 <danh8953@gmail.com>
2021-09-21 15:15:15 -0400

    Update GuessingGameServiceLayerImpl.java
    Added createAnswer method

--- GuessTheNumber/src/main/java/Main/Service/GuessingGameServiceLayerImpl.java
+++ GuessTheNumber/src/main/java/Main/Service/GuessingGameServiceLayerImpl.java
@@ -5,10 +5,25 @@
  */
 package Main.Service;
 
+import java.util.HashSet;
+import java.util.Random;
+
 /**
  *
  * @author Noah McElroy
  */
 public class GuessingGameServiceLayerImpl {
-    
+    int createAnswer(){
+        HashSet<Integer> set=new HashSet();  
+        Random rand = new Random();
+        String answer ="";
+        while(set.size() < 4){
+            int randomInt = rand.nextInt(10);
+            if (!set.contains(randomInt)){
+                set.add(randomInt);
+                answer+= randomInt;
+            }
+        }
+        return Integer.parseInt(answer);
+    }
 }
