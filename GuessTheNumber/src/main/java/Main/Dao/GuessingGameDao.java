f3617b4d33d062d7fdcbb92bd011f49b0704804f
NLRUmbra <56780974+NLRUmbra@users.noreply.github.com>
2021-09-21 16:54:10 -0400

    Update GuessingGameDao.java

--- GuessTheNumber/src/main/java/Main/Dao/GuessingGameDao.java
+++ GuessTheNumber/src/main/java/Main/Dao/GuessingGameDao.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Main.Dao;
 
 import Main.CTO.Game;
@@ -26,15 +21,4 @@ public interface GuessingGameDao {
     // true if item exists and is deleted
     boolean deleteById(int id);
     
-    
-    
-    
-    
-    /*Game addGame(int id, Game game);
-    
-    Game deleteGame(int id);
-    
-    //Game updateGame(int id);
-    
-    List<Game> listAllGames();*/
 }
