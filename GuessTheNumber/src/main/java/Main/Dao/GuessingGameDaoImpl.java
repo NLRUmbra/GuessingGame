d33aa5f6ca05921d93826f1523b7aa723af5d715
NLRUmbra <56780974+NLRUmbra@users.noreply.github.com>
2021-09-21 16:53:10 -0400

    Update GuessingGameDaoImpl.java

--- GuessTheNumber/src/main/java/Main/Dao/GuessingGameDaoImpl.java
+++ GuessTheNumber/src/main/java/Main/Dao/GuessingGameDaoImpl.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Main.Dao;
 
 import Main.CTO.Game;
@@ -67,40 +62,3 @@ public class GuessingGameDaoImpl implements GuessingGameDao{
     public boolean deleteById(int id) {
         return games.removeIf(i -> i.getId() == id);
     }
-
-    
-    
-    
-    
-    
-    
-    
-  
-    /*private Map<Integer, Game> gameList =new HashMap<>();
-
-    public GuessingGameDaoImpl() {
-    }
-
-    @Override
-    public Game addGame(int id, Game game) {
-       gameList.put(game.getId(),game);
-       return gameList.get(game.getId());
-    }
-
-    @Override
-    public Game deleteGame(int id) {
-        return gameList.remove(id);
-    }
-
-    /*@Override
-    public Game updateGame(int id) {
-        
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public List<Game> listAllGames() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }*/
-    
-}
